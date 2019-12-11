5119da6987c49eadc2819a71a2819a990009dc05
rochappy <rochappy.sun@gmail.com>
2019-12-11 21:48:32 +0800

    refactor: 💡 支持以svg文件形式导入

--- bin/svg2html.js
+++ bin/svg2html.js
@@ -4,10 +4,16 @@ const program = require('commander');
 const assert = require('assert');
 const pkg = require('../package.json');
 
+const list = (val) => {
+  return val.split(',').map((item) => {
+    return item.trim()
+  });
+}
+
 program.version(pkg.version, '-v, --version');
 
 program
-  .option('-i, --inDir <inDir>', 'svg input directory')
+  .option('-i, --inDir <inDir>', 'svg input directory', list)
   .option('-o, --outDir <outDir>', 'svg output directory')
   .option('-r, --rmAttr [rmAttr]', 'svg remove attr, for example: "fill" or "stroke|fill|opacity"', '')
   .option('-e, --exclude [exclude]', 'exclude directory, for example: "test" or "test|node_modules"', 'node_modules')
